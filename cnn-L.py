dbd976414e1aa482c5fde8ee4fe7563b41e0d160
Antreas Kasiotis <kasiotisandreas@gmail.com>
2022-01-09 13:45:47 +0000

    Implemented my cnn model for the greyscale images

---
+++ cnn-L.py
@@ -0,0 +1,99 @@
+# Importing required libraries
+import seaborn as sns
+import matplotlib.pyplot as plt
+import numpy as np
+import os
+import pandas as pd
+import tensorflow as tf
+import keras
+from imblearn.over_sampling import RandomOverSampler
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Flatten
+from keras.layers import Conv2D, MaxPooling2D
+from keras.utils import np_utils
+from sklearn.model_selection import train_test_split
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Conv2D, Flatten, Dense, MaxPool2D
+
+# import the data of images and the data from HAM_metadata.csv
+dataset_images = pd.read_csv("cancer-data/hmnist_28_28_L.csv")
+
+# defining the number of classes
+num_classes = 7
+# defining the batch size and epochs for the model
+batch_size = 128
+epochs = 10
+
+# defining the number of rows and columns representing the pixels
+img_rows = 28
+img_cols = 28
+
+# removing the 'label' column from the data frame so I only keep the image data
+images = dataset_images.drop(['label'], axis=1)
+# keeping only the label column
+labels = dataset_images['label']
+
+# Oversampling to overcome class imbalance
+oversample = RandomOverSampler()
+images, labels = oversample.fit_resample(images, labels)
+
+# resizing the images and parsing them into an array
+images = np.array(images)
+images = images.reshape(-1, 28, 28, 1)
+print('Shape of images: ', images.shape)
+
+# Normalizing the images.
+images = (images-np.mean(images))/np.std(images)
+
+# Splitting my predictive and response data into training and testing sets with an 80:20 ratio
+# while the state is set to a constant so that the splitting can be done reproducibly
+x_train, x_test, y_train, y_test = train_test_split(images, labels, random_state=1, test_size=0.20)
+
+# encoding my labels to one-hot vectors
+y_train = keras.utils.np_utils.to_categorical(y_train, num_classes)
+y_test = keras.utils.np_utils.to_categorical(y_test, num_classes)
+
+# Model building
+model = Sequential()
+model.add(Conv2D(32, kernel_size=(3, 3), activation='relu', input_shape=(img_rows, img_cols, 1)))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Conv2D(64, (3, 3), activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.40))
+model.add(Flatten())
+model.add(Dense(128, activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(num_classes, activation='softmax'))
+model.summary()
+
+callback = tf.keras.callbacks.ModelCheckpoint(filepath='trained-models/cnn-best-model-L.h5', monitor='val_acc', mode='max', verbose=1)
+
+model.compile(loss=keras.losses.categorical_crossentropy, optimizer='adam', metrics=['accuracy'])
+
+# Fitting the model
+history = model.fit(x_train, y_train, batch_size=batch_size, epochs=epochs, validation_split=0.2, callbacks=[callback])
+
+# Evaluating the model
+score = model.evaluate(x_test, y_test, verbose=0)
+print('Summary: Loss over the test dataset: %.2f, Accuracy: %.2f' % (score[0], score[1]))
+
+# Saving the final model for greyscale images
+model.save("trained-models/cnn-best-model-L.h5")
+
+# Plotting the accuracy of the model for each epoch
+plt.plot(history.history['accuracy'])
+plt.plot(history.history['val_accuracy'])
+plt.title('model accuracy')
+plt.xlabel('epoch')
+plt.ylabel('accuracy')
+plt.legend(['train', 'val'], loc='upper left')
+plt.show()
+
+# Plotting the val_loss of the model for each epoch
+plt.plot(history.history['loss'])
+plt.plot(history.history['val_loss'])
+plt.title('model loss')
+plt.xlabel('epoch')
+plt.ylabel('loss')
+plt.legend(['train', 'val'], loc='upper left')
+plt.show()
