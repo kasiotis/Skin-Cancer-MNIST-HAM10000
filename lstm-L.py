b48986f431514216d79651c72a3afc3ed9b3165c
Antreas Kasiotis <kasiotisandreas@gmail.com>
2022-01-10 16:18:51 +0000

    Implemented my lstm model for greyscale images

---
+++ lstm-L.py
@@ -0,0 +1,75 @@
+import pandas as pd
+import numpy as np
+import keras
+import tensorflow as tf
+from keras.models import Sequential
+from keras.layers import LSTM, Dense,Dropout
+from keras.datasets import mnist
+from keras.utils import np_utils
+from imblearn.over_sampling import RandomOverSampler
+from sklearn.model_selection import train_test_split
+
+# Hyper parameters
+batch_size = 128
+nb_epoch = 10
+
+# Parameters for MNIST dataset
+img_rows, img_cols = 28, 28
+num_classes = 7
+
+# Parameters for LSTM network
+nb_lstm = 64
+nb_time_steps = img_rows
+dim_input_vector = img_cols
+
+# import the data of images and the data from HAM_metadata.csv
+dataset_images = pd.read_csv("cancer-data/hmnist_28_28_L.csv")
+
+# removing the 'label' column from the data frame so I only keep the image data
+images = dataset_images.drop(['label'], axis=1)
+# keeping only the label column
+labels = dataset_images['label']
+
+# Oversampling to overcome class imbalance
+oversample = RandomOverSampler()
+images, labels = oversample.fit_resample(images, labels)
+
+# resizing the images and parsing them into an array
+images = np.array(images)
+images = images.reshape(-1, 28, 28)
+print('Shape of images: ', images.shape)
+
+# Normalizing the images.
+images = (images - np.mean(images)) / np.std(images)
+
+# Splitting my predictive and response data into training and testing sets with an 80:20 ratio
+# while the state is set to a constant so that the splitting can be done reproducibly
+x_train, x_test, y_train, y_test = train_test_split(images, labels, random_state=1, test_size=0.20)
+
+# encoding my labels to one-hot vectors
+y_train = keras.utils.np_utils.to_categorical(y_train, num_classes)
+y_test = keras.utils.np_utils.to_categorical(y_test, num_classes)
+
+print('X_train shape:', x_train.shape)
+print(x_train.shape[0], 'train samples')
+print(x_test.shape[0], 'test samples')
+
+# Build LSTM network
+model = Sequential()
+model.add(LSTM(nb_lstm, input_shape=(nb_time_steps, dim_input_vector), return_sequences=True))
+model.add(Dropout(0.5))
+model.add(LSTM(nb_lstm, return_sequences=False))
+model.add(Dropout(0.5))
+model.add(Dense(num_classes, activation='softmax'))
+model.summary()
+
+callback = tf.keras.callbacks.ModelCheckpoint(filepath='trained-models/lstm-best-model-L.h5', monitor='val_acc', mode='max', verbose=1)
+
+model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+
+# Fitting the model
+history = model.fit(x_train, y_train, epochs=nb_epoch, batch_size=batch_size, callbacks=[callback], validation_split = 0.2)
+
+# Evaluating the model
+score = model.evaluate(x_test, y_test, verbose=0)
+print('Summary: Loss over the test dataset: %.2f, Accuracy: %.2f' % (score[0], score[1]))
